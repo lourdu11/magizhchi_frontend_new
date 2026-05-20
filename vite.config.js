9bd3283d739c73ef629040b6fec120aa52e8bb14
judejos <judejos2302@gmail.com>
2026-05-21 01:08:30 +0530

    Perf: Match static skeleton srcset perfectly with SafeImage to prevent double fetching

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -12,6 +13,7 @@ export default defineConfig({
     react(),
     viteCompression({ algorithm: 'gzip', ext: '.gz' }),
     viteCompression({ algorithm: 'brotliCompress', ext: '.br' }),
+    visualizer({ open: false, filename: 'stats.html' })
   ],
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
